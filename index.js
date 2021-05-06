ae74e1a290857f056ce04f1ee0188e2f2fa8ed9e
jessica you <jessicaxytan@gmail.com>
2021-05-06 18:03:16 -0400

    commit tree e1254c3d97311483b4286e223987b6f55d73248c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let list = []
+    for(let i=0; i < names.length; i++) {
+        list.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return list
+}
+
+writeCards(names)
+
+
+function countDown(integer) {
+    let i = integer;
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+}
